a1afe4092780a43683d3e48a7e1c9c3dc5d90f91
shivangimajestic <shivangi@majestictechnologies.net>
2023-10-26 18:02:42 +0530

    commit

---
+++ src/app/recording/recording.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recording',
+  templateUrl: './recording.page.html',
+  styleUrls: ['./recording.page.scss'],
+})
+export class RecordingPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
